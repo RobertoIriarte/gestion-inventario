b5b0ef721e4ea9663459540d496c69f9fd63eabb
Roberto Iriarte <r.iriarte2010@gmail.com>
2025-02-18 23:03:34 -0300

    se agrega reporte anual

--- src/main/java/com/ventas/code/controller/ReportesController.java
+++ src/main/java/com/ventas/code/controller/ReportesController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ventas.code.dto.ReporteAnualDTO;
 import com.ventas.code.model.CabFactura;
 import com.ventas.code.model.Producto;
 import com.ventas.code.service.CabFacturaService;
@@ -61,5 +62,61 @@ public class ReportesController {
                              .mapToInt(cab -> (int) Double.parseDouble(cab.getTotal())).sum();
         
         return count;
+    }
+
+    @GetMapping("/anual")
+    public ReporteAnualDTO reporteAnual() {
+        ReporteAnualDTO anual = new ReporteAnualDTO();
+        List<CabFactura> cabeceras = cabFacturaService.obtenerTodas();
+        System.out.println(LocalDate.now().getMonthValue());
+
+        for(int i=1; i<13; i++){
+            switch (i) {
+                case 1:
+                    anual.setEnero(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==1).toList().size());
+                    break;
+                case 2:
+                    anual.setFebrero(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==2).toList().size());
+                    break;
+                case 3:
+                    anual.setMarzo(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==3).toList().size());
+                    break;
+                case 4:
+                    anual.setAbril(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==4).toList().size());
+                    break;
+                case 5:
+                    anual.setMayo(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==5).toList().size());
+                    break;
+                case 6:
+                    anual.setJunio(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==6).toList().size());
+                    break;
+                case 7:
+                    anual.setJulio(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==7).toList().size());
+                    break;
+                case 8:
+                    anual.setAgosto(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==8).toList().size());
+                    break;
+                case 9:
+                    anual.setSeptiembre(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==9).toList().size());
+                    break;
+                case 10:
+                    anual.setOctubre(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==10).toList().size());
+                    break;
+                case 11:
+                    anual.setNoviembre(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==11).toList().size());
+                    break;
+                case 12:
+                    anual.setDiciembre(cabeceras.stream().filter(cab -> cab.getFechaCreacion().getMonthValue()==12).toList().size());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Integer count = cabeceras.stream()
+                             .filter(cab ->  LocalDate.now().getMonthValue() == cab.getFechaCreacion().getMonthValue())
+                             .mapToInt(cab -> (int) Double.parseDouble(cab.getTotal())).sum();
+        
+        return anual;
     }
 }
---
+++ src/main/java/com/ventas/code/dto/ReporteAnualDTO.java
@@ -0,0 +1,27 @@
+package com.ventas.code.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReporteAnualDTO {
+
+    private Integer enero;
+    private Integer febrero;
+    private Integer marzo;
+    private Integer abril;
+    private Integer mayo;
+    private Integer junio;
+    private Integer julio;
+    private Integer agosto;
+    private Integer septiembre;
+    private Integer octubre;
+    private Integer noviembre;
+    private Integer diciembre;
+
+}
